0769ca4c71f880a73181a9d8760554cd7df55788
MafuSora <hafizh.maulana.ub@gmail.com>
2022-09-03 06:18:40 +0700

    aa

--- index.js
+++ index.js
@@ -10,12 +10,14 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Server port
-var HTTP_PORT = 5000 
+var HTTP_PORT = process.env.PORT || 4000 
 // Start server
-app.listen(HTTP_PORT, () => {
-    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
-});
+
 // Root endpoint
 app.get("/", (req, res, next) => {
     res.json({"message":"Ok"})
+});
+
+app.listen(HTTP_PORT, () => {
+    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
 });
